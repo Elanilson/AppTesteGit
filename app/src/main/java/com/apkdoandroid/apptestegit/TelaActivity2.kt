fa1c40197ce96466d6fbab097df211d3a4e69458
Elanilson de Jesus <elanilsonpp@hotmail.com>
2023-09-11 20:00:36 -0300

    tela 2 criada

---
+++ app/src/main/java/com/apkdoandroid/apptestegit/TelaActivity2.kt
@@ -0,0 +1,11 @@
+package com.apkdoandroid.apptestegit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela2)
+    }
+}
