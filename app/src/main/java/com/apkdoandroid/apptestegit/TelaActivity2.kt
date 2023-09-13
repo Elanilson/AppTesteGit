5892caac23b3fb1c324172e690da961b172e83a9
Elanilson de Jesus <elanilsonpp@hotmail.com>
2023-09-13 09:16:36 -0300

    modificado

--- app/src/main/java/com/apkdoandroid/apptestegit/TelaActivity2.kt
+++ app/src/main/java/com/apkdoandroid/apptestegit/TelaActivity2.kt
@@ -7,5 +7,7 @@ class TelaActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela2)
+        println("Mensagem para você")
+        println("modificado")
     }
 }
